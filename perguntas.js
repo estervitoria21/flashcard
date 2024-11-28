56f6fa6a6198d33194d43c776dd284b6ca1bd238
estervitoria21 <163008451+estervitoria21@users.noreply.github.com>
2024-11-27 21:59:20 -0300

    Create perguntas.js

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'Programação 2.0',
+    'O que é phyton?',
+    'O phyton é uma linguagem de progamação'
+)
+
+criaCartao(
+    'sono',
+    'O que me define?',
+    'dormir',
+)
+
+criaCartao(
+    'aluna',
+    'Quem é a melhor aluna?',
+    'ester'
+)
+
+criaCartao(
+    'escola',
+    'O que faz na escola?',
+    'nada'
+)
+
+criaCartao(
+    'amiga',
+    'quem é a melhor amiga?',
+    'sophia'
+)
+
+criaCartao(
+    'fazer',
+    'o que gosto de fazer?',
+    'dormir e mecher no celular'
+)
